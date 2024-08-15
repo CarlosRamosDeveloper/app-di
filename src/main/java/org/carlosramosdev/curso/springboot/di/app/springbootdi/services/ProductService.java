adb66ef2499fbfe706d88d66e04dd712a03f9ce2
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-15 18:07:06 +0200

    Bugfix

--- src/main/java/org/carlosramosdev/curso/springboot/di/app/springbootdi/services/ProductService.java
+++ src/main/java/org/carlosramosdev/curso/springboot/di/app/springbootdi/services/ProductService.java
@@ -3,17 +3,19 @@ package org.carlosramosdev.curso.springboot.di.app.springbootdi.services;
 import org.carlosramosdev.curso.springboot.di.app.springbootdi.models.Product;
 import org.carlosramosdev.curso.springboot.di.app.springbootdi.repositories.ProductRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ProductService {
 
     private ProductRepository repository = new ProductRepository();
-    public Stream<Product> findAll() {
+    public List<Product> findAll() {
         return repository.findAll().stream().map(p -> {
             Double increasedPrice = p.getPrice() * 1.25d;
             p.setPrice(increasedPrice.longValue());
             return p;
-        });
+        }).collect(Collectors.toList());
     }
 
     public Product findById(Long id) {
